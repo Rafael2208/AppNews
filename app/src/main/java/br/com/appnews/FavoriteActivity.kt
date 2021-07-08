4abdd0faeda9938f3d24ae52fd9451f3e7d4630b
Rafael Viana <rafaelviana544@yahoo.com>
2021-07-08 20:21:55 -0300

    activity_favoritexml

---
+++ app/src/main/java/br/com/appnews/FavoriteActivity.kt
@@ -0,0 +1,11 @@
+package br.com.appnews
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FavoriteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_favorite)
+    }
+}
