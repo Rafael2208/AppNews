b4fc9ead9fb6cd1e02b06b1997d12193f5a98aed
Rafael Viana <rafaelviana544@yahoo.com>
2021-07-09 14:51:57 -0300

    retrofit

--- app/src/main/java/br/com/appnews/network/NewsAPI.kt
+++ app/src/main/java/br/com/appnews/network/NewsAPI.kt
@@ -1,11 +1,34 @@
 package br.com.appnews.network
 
+import br.com.appnews.model.NewsResponse
+import br.com.appnews.util.Constants.Companion.API_KEY
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface NewsAPI {
 
-    @GET
+    @GET("/v2/top-headlines")
     suspend fun getBreakingNews(
+     @Query("country")
+     countryCode: String = "br",
+     @Query("page")
+     pageNumber: Int = 1,
+     @Query("apiKey")
+     apiKey: String = API_KEY,
 
-    )
+    ):Response<NewsResponse>
+
+    @GET("/v2/everything")
+    suspend fun searchNews (
+    @Query("q")
+    searchQuery: String,
+    @Query("page")
+    pageNumber: Int = 1,
+    @Query("apiKey")
+    apiKey: String = API_KEY,
+
+    ):Response<NewsResponse>
+
+
 }
---
+++ app/src/main/java/br/com/appnews/util/Constants.kt
@@ -0,0 +1,8 @@
+package br.com.appnews.util
+
+class Constants {
+    companion object{
+        const val API_KEY = "5488f01540e94a44bf1f562ff6d08429"
+        const val BASE_URL = "https://newsapi.org"
+    }
+}
